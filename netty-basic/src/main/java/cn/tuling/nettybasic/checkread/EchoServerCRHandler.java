d2178e8bb74462e3a097214b7bf1c6139020fffa
away <awaymagic@gmail.com>
2022-12-04 17:56:36 +0800

    channelReadComplete 和 channelRead 区别

--- netty-basic/src/main/java/cn/tuling/nettybasic/checkread/EchoServerCRHandler.java
+++ netty-basic/src/main/java/cn/tuling/nettybasic/checkread/EchoServerCRHandler.java
@@ -14,6 +14,15 @@ public class EchoServerCRHandler extends ChannelInboundHandlerAdapter {
     private int readCount = 0;
     private int readCompleteCount = 0;
 
+    /**
+     * channelRead 和 channelReadComplete 区别
+     * 比如 buf 长200，每次消息40，则 channelRead 执行5次，而 channelReadComplete 执行1次
+     * 报文很大或产生分包时,一次发送未发完会出现 channelReadComplete 次数 > channelRead
+     *
+     * 总结: channelReadComplete 读物 buf 则执行一次，channelRead 在处理完半包后 和应用消息数量一致
+     * 推荐使用 channelRead
+     */
+
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         //ByteBuf in = (ByteBuf)msg;
